82e12a733c05c223a8e460b3e5d940047896a8f4
ArielArapa <85938683+ArielArapa@users.noreply.github.com>
2023-05-19 12:23:23 +0000

    delete

--- main/calculo.py
+++
@@ -1,4 +0,0 @@
-x = 10
-
-def hola(x):
-    return f"Hola {x}"
--- main/main.py
+++
@@ -1,9 +0,0 @@
-import calculo
-
-print(calculo.x)
-
-#hola es una una funcion de calculo
-# function hola (x: any) -> str
-# hola es una funcion x es una tipo de dato de cualquiera y la funcion te retorna un str
-saludo = calculo.hola("Matias")
-print(saludo)
